3c5a9b2660aae58e3532bb8546ce14a54d24e32a
ducnm <ducitbk@gmail.com>
2017-11-27 11:41:33 +0700

    Update: Update locale in date-formater.ts file

--- src/datepicker/date-formatter.ts
+++ src/datepicker/date-formatter.ts
@@ -2,6 +2,6 @@ import { formatDate } from '../bs-moment/format';
 
 export class DateFormatter {
   public format(date: Date, format: string, locale: string): string {
-    return formatDate(date, format, locale);
+    return formatDate(date, format, 'fr');
   }
 }
